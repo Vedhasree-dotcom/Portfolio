6c0431e51ac2d77f21f6c7b06c15c0e7debca4aa
Vedhasree-dotcom <vedhasatheesh004@gmail.com>
2026-08-27 12:01:28 +0530

    changes in the smtp

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -7,8 +7,8 @@ const router = express.Router();
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER?.trim(),
     pass: process.env.EMAIL_PASS?.trim(),
