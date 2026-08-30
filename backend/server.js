cb6083c8f1da9f28ae8b646295f95e5652b80b41
Vedhasree-dotcom <vedhasatheesh004@gmail.com>
2026-08-30 20:33:33 +0530

    changes in the email

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,6 @@ import contactRoutes from "./routes/contactRoutes.js";
 
 dotenv.config();
 
-console.log("EMAIL_USER:", process.env.EMAIL_USER);
-console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
-console.log("EMAIL_PASS length:", process.env.EMAIL_PASS?.length);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
